0c8634ddf46ee409e251ff428d0672d40b2e48ad
sanchitadevi <thesanchitadevi@gmail.com>
2023-03-30 12:22:38 +0600

    oder api

--- controllers/order/index.js
+++ controllers/order/index.js
@@ -3,6 +3,8 @@ const OrderLine = require("../../models/OrderLine");
 const Item = require("../../models/Item");
 const Stitch = require("../../models/Stitch");
 const ErrorResponse = require("../../utils/errorResponse");
+const { fieldsQuery, queryObjectBuilder } = require("../../utils/fieldsQuery");
+const { default: mongoose } = require("mongoose");
 
 exports.create = async (req, res, next) => {
 	// Get Values
@@ -68,5 +70,79 @@ exports.create = async (req, res, next) => {
 	} catch (error) {
 		// Send Error Response
 		return next(error);
+	}
+};
+
+exports.getAll = async (req, res, next) => {
+	const { customer} = req.query;
+
+	try {
+		res.status(200).json({
+			success: true,
+			message: "Order list fetched successfully",
+			...(await Order.paginate(
+				{
+					...(req.search && {
+						$or: [
+							...queryObjectBuilder(
+								req.search,
+								[],
+								true
+							)
+						],
+					}),
+					...fieldsQuery({
+						customer
+					}),
+				},
+				{
+					...req.pagination,
+					populate: [
+						{
+							path: "customer",
+						},
+					],
+					customLabels: {
+						docs: "data",
+						totalDocs: "total",
+					},
+				}
+			))
+		});
+
+		// On Error
+	} catch (error) {
+		// Send Error Response
+		next(error);
+	}
+};
+
+exports.byID = async (req, res, next) => {
+	// Get Values
+	const { order_id } = req.params;
+
+	// mongoose.Types.ObjectId.isValid(id)
+	if (!order_id || !mongoose.Types.ObjectId.isValid(order_id))
+		return next(new ErrorResponse("Please provide valid order id", 400));
+
+	try {
+		const order = await Order.findById(order_id)
+			.populate([
+				{
+					path: "customer",
+				}
+			])
+
+		if (!order) return next(new ErrorResponse("No order found", 404));
+
+		res.status(200).json({
+			success: true,
+			data: order,
+		});
+
+		// On Error
+	} catch (error) {
+		// Send Error Response
+		next(error);
 	}
 };
