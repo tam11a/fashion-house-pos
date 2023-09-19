898c64e59dd5ca4cb878a6b4ba8fdfded5016956
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-19 12:45:20 +0600

    added: range report

--- controllers/report/index.js
+++ controllers/report/index.js
@@ -6,88 +6,133 @@ const Order = require("../../models/Order");
 const Product = require("../../models/Product");
 
 exports.global = async (req, res, next) => {
-	const totalSales = await Order.aggregate([
-		{
-			$group: {
-				_id: null,
-				total: { $sum: "$total" },
-			},
-		},
-	]);
-	const totalDiscount = await Order.aggregate([
-		{
-			$group: {
-				_id: null,
-				total: { $sum: "$discount" },
-			},
-		},
-	]);
+	// const totalSales = await Order.aggregate([
+	// 	{
+	// 		$group: {
+	// 			_id: null,
+	// 			total: { $sum: "$total" },
+	// 		},
+	// 	},
+	// ]);
+	// const totalDiscount = await Order.aggregate([
+	// 	{
+	// 		$group: {
+	// 			_id: null,
+	// 			total: { $sum: "$discount" },
+	// 		},
+	// 	},
+	// ]);
 
-	const totalDue = await Order.aggregate([
-		{
-			$match: {
-				$expr: {
-					$gte: [
-						{
-							$subtract: [
-								{ $subtract: ["$total", "$discount"] },
-								{
-									$sum: "$transaction.amount",
-								},
-							],
+	// const totalDue = await Order.aggregate([
+	// 	{
+	// 		$match: {
+	// 			$expr: {
+	// 				$gte: [
+	// 					{
+	// 						$subtract: [
+	// 							{ $subtract: ["$total", "$discount"] },
+	// 							{
+	// 								$sum: "$transaction.amount",
+	// 							},
+	// 						],
+	// 					},
+	// 					0,
+	// 				],
+	// 			},
+	// 		},
+	// 	},
+	// 	{
+	// 		$group: {
+	// 			_id: null,
+	// 			total: {
+	// 				$sum: {
+	// 					$subtract: [
+	// 						{ $subtract: ["$total", "$discount"] },
+	// 						{
+	// 							$sum: "$transaction.amount",
+	// 						},
+	// 					],
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// ]);
+
+	// const totalPaid = await Order.aggregate([
+	// 	{
+	// 		$group: {
+	// 			_id: null,
+	// 			total: {
+	// 				$sum: {
+	// 					$sum: "$transaction.amount",
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// ]);
+	try {
+		res.status(200).json({
+			success: true,
+			message: "Global Report",
+			data: {
+				totalBranches: await Branch.countDocuments(),
+				totalEmployees: await Admin.countDocuments(),
+				totalCustomers: await Customer.countDocuments(),
+				totalProducts: await Product.countDocuments(),
+				totalOrders: await Order.countDocuments(),
+				totalAvailableItems: await Item.countDocuments({
+					orderLine: { $eq: null },
+				}),
+				totalItems: await Item.countDocuments(),
+				// totalSales: totalSales[0].total,
+				// totalDue: totalDue[0].total,
+				// totalPaid: totalPaid[0].total,
+				// totalDiscount: totalDiscount[0].total,
+			},
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.range = async (req, res, next) => {
+	try {
+		const { branch, fromDate, toDate } = req.query;
+
+		res.status(200).json({
+			success: true,
+			message: "Range Report",
+			data: {
+				newCustomers: await Customer.countDocuments({
+					...((fromDate || toDate) && {
+						createdAt: {
+							...(fromDate && {
+								$gte: fromDate,
+							}),
+							...(toDate && {
+								$lte: toDate,
+							}),
 						},
-						0,
-					],
-				},
+					}),
+				}),
+				totalOrders: await Order.countDocuments({
+					...(branch && {
+						branch,
+					}),
+					...((fromDate || toDate) && {
+						createdAt: {
+							...(fromDate && {
+								$gte: fromDate,
+							}),
+							...(toDate && {
+								$lte: toDate,
+							}),
+						},
+					}),
+				}),
 			},
-		},
-		{
-			$group: {
-				_id: null,
-				total: {
-					$sum: {
-						$subtract: [
-							{ $subtract: ["$total", "$discount"] },
-							{
-								$sum: "$transaction.amount",
-							},
-						],
-					},
-				},
-			},
-		},
-	]);
-
-	const totalPaid = await Order.aggregate([
-		{
-			$group: {
-				_id: null,
-				total: {
-					$sum: {
-						$sum: "$transaction.amount",
-					},
-				},
-			},
-		},
-	]);
-
-	res.status(200).json({
-		success: true,
-		message: "Global Report",
-		data: {
-			totalBranches: await Branch.countDocuments(),
-			totalEmployees: await Admin.countDocuments(),
-			totalCustomers: await Customer.countDocuments(),
-			totalProducts: await Product.countDocuments(),
-			totalOrders: await Order.countDocuments(),
-			totalAvailableItems: await Item.countDocuments({
-				orderLine: { $eq: null },
-			}),
-			totalItems: await Item.countDocuments(),
-			totalSales: totalSales[0].total,
-			totalDue: totalDue[0].total,
-			totalPaid: totalPaid[0].total,
-			totalDiscount: totalDiscount[0].total,
-		},
-	});
+		});
+	} catch (error) {
+		next(error);
+	}
 };
--- routes/report/index.js
+++ routes/report/index.js
@@ -21,4 +21,31 @@ router
 	.route("/global")
 	.get(protect, require("../../controllers/report").global);
 
+/**
+ * @swagger
+ * /api/report/range:
+ *  get:
+ *    tags: [Report]
+ *    summary: Get Range Report
+ *    security:
+ *      - bearer: []
+ *    parameters:
+ *      - in: query
+ *        name: branch
+ *        type: string
+ *      - in: query
+ *        name: fromDate
+ *        type: date
+ *      - in: query
+ *        name: toDate
+ *        type: date
+ *    responses:
+ *      200:
+ *        description: Get successful
+ *      400:
+ *        description: Bad Request
+ *
+ */
+router.route("/range").get(protect, require("../../controllers/report").range);
+
 module.exports = router;
