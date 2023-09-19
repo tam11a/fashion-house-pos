bed1ca2ec331e670d711133649e724dbcfec1206
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-19 19:20:59 +0600

    added: top 5 salesman report

--- controllers/report/index.js
+++ controllers/report/index.js
@@ -130,7 +130,7 @@ exports.range = async (req, res, next) => {
 			{
 				$group: {
 					_id: null,
-					total: { $sum: "$total" },
+					total: { $sum: { $subtract: ["$total", "$discount"] } },
 				},
 			},
 		]);
@@ -229,6 +229,129 @@ exports.range = async (req, res, next) => {
 			},
 		]);
 
+		const mfs = ["COD", "Card", "Cash", "bKash", "Nagad", "Rocket"];
+
+		const mfsReport = await Order.aggregate([
+			{
+				$match: {
+					...(branch && {
+						branch: {
+							$eq: ObjectId(branch),
+						},
+					}),
+				},
+			},
+			{
+				$match: {
+					$and: [
+						{
+							createdAt: {
+								...(fromDate && {
+									$gte: new Date(fromDate),
+								}),
+								...(toDate && {
+									$lte: new Date(toDate),
+								}),
+							},
+						},
+					],
+				},
+			},
+			{
+				$unwind: "$transaction", // Unwind the transaction array
+			},
+			{
+				$group: {
+					_id: "$transaction.method", // Group by payment method
+					totalAmount: { $sum: "$transaction.amount" }, // Calculate the total amount for each method
+				},
+			},
+			{
+				$project: {
+					_id: 0, // Exclude the default _id field
+					method: "$_id", // Rename _id to method
+					totalAmount: 1, // Include the total amount
+				},
+			},
+		]);
+
+		const top5Salesman = await Order.aggregate([
+			{
+				$match: {
+					...(branch && {
+						branch: {
+							$eq: ObjectId(branch),
+						},
+					}),
+				},
+			},
+			{
+				$match: {
+					$and: [
+						{
+							createdAt: {
+								...(fromDate && {
+									$gte: new Date(fromDate),
+								}),
+								...(toDate && {
+									$lte: new Date(toDate),
+								}),
+							},
+						},
+					],
+				},
+			},
+			{
+				$group: {
+					_id: "$salesman", // Group by salesman
+					totalSalesQuantity: { $sum: 1 }, // Calculate total sales quantity
+					totalSalesAmount: { $sum: "$total" }, // Calculate total sales amount
+				},
+			},
+			{
+				$sort: { totalSalesAmount: -1 }, // Sort by total sales amount in descending order
+			},
+			{
+				$limit: 10, // Limit the result to the top 10 salesmen
+			},
+			// {
+			// 	$project: {
+			// 		_id: 0,
+			// 		salesman: "$_id",
+			// 	},
+			// },
+			{
+				$project: {
+					_id: 0,
+					salesman: "$_id",
+					salesmanData: {
+						// $arrayElemAt: ["$salesmanData", 0], // Get the name of the salesman
+						$cond: {
+							if: { $eq: ["$salesmanData", []] },
+							then: null,
+							else: {
+								$concat: [
+									{ $arrayElemAt: ["$salesmanData.firstName", 0] },
+									" ",
+									{ $arrayElemAt: ["$salesmanData.lastName", 0] },
+								],
+							},
+						},
+					},
+					totalSalesQuantity: 1,
+					totalSalesAmount: 1,
+				},
+			},
+			{
+				$lookup: {
+					from: "admins", // Assuming the employee data is in the "Admin" collection
+					localField: "salesman",
+					foreignField: "_id",
+					as: "salesmanData",
+				},
+			},
+		]);
+
 		res.status(200).json({
 			success: true,
 			message: "Range Report",
@@ -263,6 +386,14 @@ exports.range = async (req, res, next) => {
 				totalSales: totalSales?.[0]?.total || 0,
 				totalDue: totalDue?.[0]?.total || 0,
 				totalPettyCash: totalPettyCash?.[0]?.total || 0,
+				MFSReport: mfs.reduce((a, b) => {
+					const data = mfsReport.find((item) => item.method === b);
+					return {
+						...a,
+						[b]: data?.totalAmount || 0,
+					};
+				}, {}),
+				topSalesman: top5Salesman,
 			},
 		});
 	} catch (error) {
